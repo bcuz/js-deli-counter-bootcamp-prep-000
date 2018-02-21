5b0ef94d9151869ba106beba12dac91a9b0094a1
adam N <adam.nawroc@gmail.com>
2018-02-21 18:41:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(line) {
   if (line.length === 0) {
     return "The line is currently empty."
   } else {
-var string = `The line is currently: `
+    var string = `The line is currently: `
     for (var i = 0; i < line.length; i++) {
 
       if (i !== line.length - 1) {
