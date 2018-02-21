3796cf0b94b1a3e6e5e69da5c24140d58cba2fcb
adam N <adam.nawroc@gmail.com>
2018-02-21 18:33:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name) + 1} in line.`
 
 }
-function nowServing() {
+function nowServing(line) {
   
 }
